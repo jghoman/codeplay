077ba90a3ac0fc762598d40464dc85ce54bb1793
Jakob Homan <jghoman@gmail.com>
2015-01-02 18:16:56 -0800

    Basic character class.  Completely immutable... hmmmmm...

--- rust/structs/src/main.rs
+++ rust/structs/src/main.rs
@@ -1,38 +1,51 @@
+use std::cmp::max;
+use std::cell::Cell;
 
-struct Circle {
-  x: f64,
-  y: f64,
-  radius: f64,
+struct Character<'a> { 
+  name: &'a str,
+  max_hit_points: i32,
+  curr_hit_points: i32,
+  points_per_attack: i32,
 }
 
-impl Circle {
-  fn new(x: f64, y: f64, radius: f64) -> Circle {
-    Circle {
-      x: x,
-      y: y,
-      radius: radius,
+impl<'a> Character<'a> {
+  fn new(name:  &'a str , max_hit_points: i32, points_per_attack: i32) -> Character<'a> {
+    Character {name: name, 
+      max_hit_points: max_hit_points,
+      curr_hit_points: max_hit_points,
+      points_per_attack: points_per_attack
     }
   }
 
-//  fn area(&self) -> f64 {
-//    std::f64::consts::PI * (self.radius * self.radius)
-//  }
-}
+  fn is_alive(&self) -> bool {
+    self.curr_hit_points > 0
+  }
 
-trait HasArea {
-  fn area(&self) -> f64;
-}
+  fn decrease_health(&self, hit_points:i32) -> Character {
+    Character {name: self.name,
+      max_hit_points: self.max_hit_points,
+      curr_hit_points: max(0, self.curr_hit_points - hit_points),
+      points_per_attack: self.points_per_attack
+    }
+  } 
 
-impl HasArea for Circle {
-  fn area(&self) -> f64 {
-    std::f64::consts::PI * (self.radius * self.radius)
+  fn attack(&self, c:Character) {
   }
 }
 
-fn main() {
-  let c = Circle { x: 0.0, y: 0.0, radius: 2.0 };
-  println!("{}", c.area());
+#[test]
+fn decrease_health_works_correctly() {
+  let starting_health = 50;
+  let bob = Character::new("Bob", starting_health, 32);
+  let dead_bob = bob.decrease_health(starting_health * 2);
+  assert!(dead_bob.curr_hit_points == 0);
+  assert!(dead_bob.is_alive() == false);
 
-  let c2 = Circle::new(4.3, 2.7, 9.6);
-  println!("c2 = {}", c2.area());
+  let surviving_bob = bob.decrease_health(starting_health - 1);
+  println!("{}", surviving_bob.curr_hit_points);
+  assert!(surviving_bob.curr_hit_points == 1);
+  assert!(surviving_bob.is_alive() == true);
+}
+
+fn main() {
 }
