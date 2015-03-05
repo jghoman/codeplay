a75727c941f8211c72482591902e03e96469042c
Jakob Homan <jghoman@gmail.com>
2015-03-04 22:25:08 -0800

    go: A bit more.

--- go/helloworld.go
+++ go/helloworld.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
-  fmt.Println("Howdy.")
+	fmt.Println("Howdy.")
+	fmt.Println("My favorite number is ", rand.Intn(10))
 }
