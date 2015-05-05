bf008cb0b1affa44fd34f4994868bb1920b29a5f
Jakob Homan <jghoman@gmail.com>
2015-05-05 15:50:43 -0700

    Java: datastructures: working a bit more.

--- java/datastructures/src/main/java/homan/BinaryTree.java
+++ java/datastructures/src/main/java/homan/BinaryTree.java
@@ -1,14 +1,16 @@
-package homan.datastructures;
+package homan;
 
 import java.util.*;
 
 public class BinaryTree<T extends Comparable> {
+
   public static class Node<T extends Comparable> {
     T element;
     Node<T> left;
     Node<T> right;
 
     public Node(T element, Node<T> left, Node<T> right) {
+      Utils.notNull(element);
       this.element = element;
       this.left = left;
       this.right = right;
@@ -45,20 +47,43 @@ public class BinaryTree<T extends Comparable> {
         .append("]")
         .toString();
     }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (!(o instanceof Node)) return false;
+
+      Node<?> node = (Node<?>) o;
+
+      if (!element.equals(node.element)) return false;
+      if (left != null ? !left.equals(node.left) : node.left != null)
+        return false;
+      if (right != null ? !right.equals(node.right) : node.right != null)
+        return false;
+
+      return true;
+    }
+
+    @Override
+    public int hashCode() {
+      int result = element.hashCode();
+      result = 31 * result + (left != null ? left.hashCode() : 0);
+      result = 31 * result + (right != null ? right.hashCode() : 0);
+      return result;
+    }
   }
 
   private Node<T> root = null;
 
   public BinaryTree() { /** Nothing to do **/ }
 
-  private void notNull(Object o) throws IllegalArgumentException {
-    if(o == null) {
-      throw new IllegalArgumentException("Value cannot be null");
-    }
+  // Visible for testing
+  BinaryTree(Node<T> root) {
+    this.root = root;
   }
-  
+
   public void insert(T t) {
-    notNull(t);
+    Utils.notNull(t);
     if(root == null) {
       root = new Node<T>(t);
     } else {
@@ -86,7 +111,7 @@ public class BinaryTree<T extends Comparable> {
   }
 
   public interface TreeVisitor<T extends Comparable> {
-    public void visit(Node<T> element);
+    void visit(Node<T> element);
   }
   
   public class PrintVisitor implements TreeVisitor<T> {
@@ -160,4 +185,79 @@ public class BinaryTree<T extends Comparable> {
     }
   }
 
+  /**
+   * Find and disconnect the minimum node in the tree
+   */
+  protected Node<T> removeMinNode() {
+    // Empty tree
+    if(root == null) {
+      return null;
+    }
+
+    // Just root tree
+    if(root.getRight() == null && root.getLeft() == null) {
+      Node<T> toReturn = root;
+      root = null;
+      return toReturn;
+    }
+
+    Node<T> cursor = root;
+    while(cursor.getRight().getRight() != null) {
+      cursor = cursor.getRight();
+    }
+
+    Node<T> toReturn = cursor.getRight();
+    cursor.setRight(null);
+    return toReturn;
+  }
+
+  public boolean remove(T t) {
+    if(root == null) {
+      return false;
+    }
+
+    if(root.element.equals(t)) {
+      return removeRoot();
+    }
+    return remove(t, root, null);
+  }
+
+  private boolean removeRoot() {
+    if(root.getLeft() == null && root.getRight() == null) {
+      root = null;
+      return true;
+    }
+
+    if(root.getLeft() != null && root.getRight() == null) {
+      root = root.getLeft();
+      return true;
+    }
+
+    if(root.getLeft() == null && root.getRight() != null) {
+      root = root.getRight();
+      return true;
+    }
+
+    return true;
+  }
+
+  private boolean remove(T t, Node<T> node, Node<T> parent) {
+    return true;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof BinaryTree)) return false;
+
+    BinaryTree<?> that = (BinaryTree<?>) o;
+
+    return !(root != null ? !root.equals(that.root) : that.root != null);
+
+  }
+
+  @Override
+  public int hashCode() {
+    return root != null ? root.hashCode() : 0;
+  }
 }
--- java/datastructures/src/test/java/homan/BinaryTreeTest.java
+++ java/datastructures/src/test/java/homan/BinaryTreeTest.java
@@ -1,4 +1,4 @@
-package homan.datastructures;
+package homan;
 
 import java.util.*;
 import org.junit.*;
@@ -46,6 +46,7 @@ public class BinaryTreeTest {
   public void insertWorks() {
     BinaryTree<String> bt = getBalancedTree();
     bt.printInOrder();
+    // TODO: Actually do a test here
   }
 
   @Test
@@ -58,11 +59,40 @@ public class BinaryTreeTest {
       String i = iter.next();
       System.out.println("howdy : " + i);
     }
+    // TODO: Actually do a test here
+  }
+
+  @Test
+  public void removeMinNodeEmptyTree() {
+    BinaryTree<String> bt = new BinaryTree<>();
+
+    assertNull(bt.removeMinNode());
+  }
+
+  @Test
+  public void removeMinNodeOneLevelTree() {
+    BinaryTree<String> bt = new BinaryTree<>();
+
+    bt.insert("m");
+
+    assertEquals(new BinaryTree.Node<>("m"), bt.removeMinNode());
+    assertTrue(bt.isEmpty());
+  }
+
+  @Test
+  public void removeMinNodeTwoLevelTree() {
+    BinaryTree<String> bt = new BinaryTree<>();
+
+    bt.insert("m");
+    bt.insert("a");
+
+    assertEquals(new BinaryTree.Node<>("a"), bt.removeMinNode());
+    assertTrue(bt.contains("m"));
   }
 
   @Test
   public void inOrderTraversalWorks() {
-    final int NUM_LETTERS = 10;
+    final int NUM_LETTERS = 9;
     String[] alphabet = new String[NUM_LETTERS];
     Collection<String> expected = new ArrayList<String>(NUM_LETTERS);
 
@@ -95,5 +125,66 @@ public class BinaryTreeTest {
     }
     System.out.println("Counter = " + counter);
   }
+
+  @Test
+  public void removalFromEmptyTreeWorks() {
+    BinaryTree<String> bt = new BinaryTree<>();
+
+    assertFalse(bt.remove("not in here"));
+  }
+
+  @Test
+  public void removalFromTreeOfJustToBeRemovedWorks1() {
+    // Remove the root, which has no children
+    BinaryTree<String> bt = new BinaryTree<>();
+
+    bt.insert("howdy");
+
+    assertTrue(bt.contains("howdy"));
+    assertTrue(bt.remove("howdy"));
+    assertFalse(bt.contains("howdy"));
+  }
+
+  @Test
+  public void removalFromTreeOfJustToBeRemovedWorks2() {
+    // Remove the root, which has just a left child
+    BinaryTree<String> bt = new BinaryTree<>();
+    bt.insert("m");
+    bt.insert("a");
+
+    assertTrue(bt.contains("m"));
+    assertTrue(bt.remove("m"));
+    assertFalse(bt.contains("m"));
+    assertTrue(bt.contains("a"));
+  }
+
+  @Test
+  public void removalFromTreeOfJustToBeRemovedWorks3() {
+    // Remove the root, which has just a right child
+    BinaryTree<String> bt = new BinaryTree<>();
+    bt.insert("m");
+    bt.insert("z");
+
+    assertTrue(bt.contains("m"));
+    assertTrue(bt.remove("m"));
+    assertFalse(bt.contains("m"));
+    assertTrue(bt.contains("z"));
+  }
+
+  @Test
+  public void removalFromTreeOfJustToBeRemovedWorks4() {
+    // Remove the root, which has just both children
+    BinaryTree<String> bt = new BinaryTree<>();
+    bt.insert("m");
+    bt.insert("z");
+    bt.insert("a");
+
+    assertTrue(bt.contains("m"));
+    assertTrue(bt.remove("m"));
+    assertFalse(bt.contains("m"));
+    assertTrue(bt.contains("z"));
+    assertTrue(bt.contains("a"));
+  }
+
 
 }
