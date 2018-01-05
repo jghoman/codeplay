7bb56faeec425ee6fd3ed4f342d80a26c5e9e926
Jakob Homan <jghoman@gmail.com>
2018-01-04 16:04:28 -0800

    rust: oreilly: chap09: binary tree.

---
+++ rust/oreilly/chap09/binarytree/src/main.rs
@@ -0,0 +1,44 @@
+
+use std::fmt::Display;
+
+enum BinaryTree<T> {
+    Empty,
+    NonEmpty(Box<TreeNode<T>>)
+}
+
+struct TreeNode<T> {
+    element: T,
+    left: BinaryTree<T>,
+    right: BinaryTree<T>
+}
+
+impl<T: Ord + Display> BinaryTree<T> {
+
+    fn add(&mut self, value:T) {
+        match *self {
+            BinaryTree::Empty =>
+                *self = BinaryTree::NonEmpty(Box::new(TreeNode { element: value, left: BinaryTree::Empty, right: BinaryTree::Empty})),
+            BinaryTree::NonEmpty(ref mut node) =>
+                if value <= node.element {
+                    node.left.add(value)
+                } else {
+                    node.right.add(value)
+                }
+        }
+    }
+
+
+    fn new() -> BinaryTree<T> {
+        BinaryTree::Empty
+    }
+}
+
+
+fn main() {
+    println!("Hello, world!");
+    let mut tree = BinaryTree::<i32>::new();
+
+    tree.add(5);
+    tree.add(3);
+    tree.add(1);
+}
