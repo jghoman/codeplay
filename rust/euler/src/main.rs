67e166c4e35236acda33cf2938532e67d5010f96
Jakob Homan <jghoman@gmail.com>
2018-01-28 18:49:15 -0800

    rust: euler: refactor

--- rust/euler/src/main.rs
+++ rust/euler/src/main.rs
@@ -1,7 +1,7 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
-fn sum_5_or_3_multiples(upper_bound: u64) -> u64 {
+fn p01_sum_5_or_3_multiples(upper_bound: u64) -> u64 {
     let mut sum = 0;
     for i in 0 .. upper_bound {
         if (i % 3 == 0) || (i % 5 == 0) {
@@ -13,16 +13,18 @@ fn sum_5_or_3_multiples(upper_bound: u64) -> u64 {
 }
 
 #[test]
-fn test_sum_5_or_3_multiples() {
+fn p01_test_sum_5_or_3_multiples() {
     assert_eq!(sum_5_or_3_multiples(10), 23);
 }
 
 #[test]
-fn test_expected_answer() {
+fn p01_test_expected_answer() {
     assert_eq!(sum_5_or_3_multiples(1000), 233168);
 }
 
+
+
 fn main() {
-    let result = sum_5_or_3_multiples(1000);
+    let result = p01_sum_5_or_3_multiples(1000);
     println!("Sum of all the multiple numbers of 3 or 5 below 1000 is {}", result);
 }
