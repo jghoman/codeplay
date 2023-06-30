f35248482529d4f91b61c9a0e1cb7cd517007c9a
Jakob Homan <jghoman@gmail.com>
2023-06-30 15:47:08 -0700

    Text.

--- web/canvas/app.js
+++ web/canvas/app.js
@@ -4,27 +4,8 @@ window.onload = function() {
 
     console.log(context);
 
-    //context.lineWidth = 20;
-    // context.strokeStyle = "red";
+    context.fillStyle = "black";
+    context.font = "24px sans-serif";
+    context.fillText("Hello, world!", 50, 50);
 
-    // context.strokeRect(50, 50, 200, 200);
-    // context.beginPath();
-    // context.moveTo(50, 50);
-    // context.lineTo(150, 150);
-    // context.stroke();
-
-    // context.lineWidth = 20;
-
-    // const gradient = context.createLinearGradient(50, 50, 200, 200);
-    // gradient.addColorStop(0, "red");
-    // gradient.addColorStop(1, "blue");
-    // context.strokeStyle = gradient;
-
-    // // Draw a rectangle with a gradient stroke
-    // context.strokeRect(50, 50, 200, 200);
-
-
-    context.beginPath();
-    context.arc(100, 100, 50, 0, Math.PI, true);
-    context.stroke();
 }
