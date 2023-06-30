02d9d6f3e24354dacba7c34a4646c0c50db9cac0
Jakob Homan <jghoman@gmail.com>
2023-06-30 15:39:26 -0700

    arc.

--- web/canvas/app.js
+++ web/canvas/app.js
@@ -4,7 +4,7 @@ window.onload = function() {
 
     console.log(context);
 
-    context.lineWidth = 20;
+    //context.lineWidth = 20;
     // context.strokeStyle = "red";
 
     // context.strokeRect(50, 50, 200, 200);
@@ -13,13 +13,18 @@ window.onload = function() {
     // context.lineTo(150, 150);
     // context.stroke();
 
-    context.lineWidth = 20;
+    // context.lineWidth = 20;
 
-    const gradient = context.createLinearGradient(50, 50, 200, 200);
-    gradient.addColorStop(0, "red");
-    gradient.addColorStop(1, "blue");
-    context.strokeStyle = gradient;
+    // const gradient = context.createLinearGradient(50, 50, 200, 200);
+    // gradient.addColorStop(0, "red");
+    // gradient.addColorStop(1, "blue");
+    // context.strokeStyle = gradient;
 
-    // Draw a rectangle with a gradient stroke
-    context.strokeRect(50, 50, 200, 200);
+    // // Draw a rectangle with a gradient stroke
+    // context.strokeRect(50, 50, 200, 200);
+
+
+    context.beginPath();
+    context.arc(100, 100, 50, 0, Math.PI, true);
+    context.stroke();
 }
