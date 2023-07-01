b073b7cf88867115da72788a09e4ffaf742f8525
Jakob Homan <jghoman@gmail.com>
2023-07-01 15:46:53 -0700

    A bit of bouncing

--- web/canvas/app.js
+++ web/canvas/app.js
@@ -4,48 +4,67 @@ window.onload = function() {
 
     console.log(context);
 
-    let firstx = 0;
-    let secondx = canvas.width;
-    let direction = "right"
+    let firstx = canvas.width / 2;
+    let firsty = canvas.height / 2;
+
+    let firstvx = 5;
+    let firstvy = 2;
+
+    const firstradius = 25;
+
+    let secondx = 30;
+    let secondy = 30;
+
+    let secondvx = 7;
+    let secondvy = 9;
+
+    const secondradius = 30;
+
+
     function animate() {
-        // Clear the canvas
         context.clearRect(0, 0, canvas.width, canvas.height);
 
+        // Update ball position and velocity
+        firstx += firstvx;
+        firsty += firstvy;
+        secondx += secondvx;
+        secondy += secondvy;
 
-        // Update the circle's position
-        
-        if (direction === "right") {
-            firstx += 2;
-            secondx -= 1;
-            if(firstx + 25 > canvas.width) {
-                direction = "left"
-            }
+        // Check for collision
+        if((secondx - firstx) * (secondx - firstx) + (secondy - firsty) * (secondy - firsty) <= (firstradius + secondradius) * (firstradius + secondradius)) {
+            firstvx = -firstvx;
+            firstvy = -firstvy;
+            secondvx = -secondvx;
+            secondvy = -secondvy;
         } else {
-            firstx -= 2;
-            secondx += 1;
-            if(firstx <= 0) {
-                direction = "right"
+            if(firstx + firstradius > canvas.width || firstx - firstradius < 0) {
+                firstvx = -firstvx;
+            }
+            if(firsty + firstradius > canvas.height || firsty - firstradius < 0) {
+                firstvy = -firstvy;
+            }
+
+            if(secondx + secondradius > canvas.width || secondx - secondradius < 0) {
+                secondvx = -secondvx;
+            }
+            if(secondy + secondradius > canvas.height || secondy - secondradius < 0) {
+                secondvy = -secondvy;
             }
         }
-        
-
-
-
-        // Draw the circle
+        // Draw the balls
         context.beginPath();
-        context.arc(firstx, 50, 25, 0, 2 * Math.PI);
-        context.fillStyle = "pink";
+        context.arc(firstx, firsty, firstradius, 0, 2 * Math.PI);
+        context.fillStyle = "red";
         context.fill();
 
-        // Draw another circle
         context.beginPath();
-        context.arc(secondx, 250, 45, 0, 2 * Math.PI);
-        context.fillStyle = "lightblue";
+        context.arc(secondx, secondy, secondradius, 0, 2 * Math.PI);
+        context.fillStyle = "blue";
         context.fill();
 
-        // Schedule the next frame
         requestAnimationFrame(animate);
     }
+
 
     // Start the animation
     animate();
