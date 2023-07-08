a7f2a01620343c7889726507f2277977953cd747
Jakob Homan <jghoman@gmail.com>
2023-07-08 00:51:01 -0600

    Smoother animation. Reformat.

--- web/canvas/panning.js
+++ web/canvas/panning.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
     const canvas = document.getElementById("my-canvas");
     const context = canvas.getContext("2d");
 
@@ -12,32 +12,32 @@ window.onload = function() {
 
     const camera = {
         x: 0,
-        y: 0, 
+        y: 0,
         width: canvas.width,
         height: canvas.height,
         cameraX: 0,
         cameraY: 0,
 
-        update: function() {
+        update: function () {
             const div = 2;
             // Update camera position
             this.cameraX = runner.x - this.width / div;
             this.cameraY = runner.y - this.height / div;
 
             // Keep camera within bounds of game world
-            if(this.cameraX < 0) {
+            if (this.cameraX < 0) {
                 this.cameraX = 0;
             }
 
-            if(this.cameraX + this.width > bgImg.width) {
+            if (this.cameraX + this.width > bgImg.width) {
                 this.cameraX = bgImg.width - this.width;
             }
 
-            if(this.cameraY < 0) {
+            if (this.cameraY < 0) {
                 this.cameraY = 0;
             }
 
-            if(this.cameraY + this.height > bgImg.height) {
+            if (this.cameraY + this.height > bgImg.height) {
                 this.cameraY = bgImg.height - this.height;
             }
 
@@ -46,7 +46,7 @@ window.onload = function() {
     };
 
     const background = {
-        draw:function() {
+        draw: function () {
             // Draw background image with camera offset
             context.drawImage(bgImg, camera.cameraX, camera.cameraY, camera.width, camera.height, 0, 0, camera.width, camera.height);
         },
@@ -55,47 +55,59 @@ window.onload = function() {
     const runner = {
         frameWidth: 165,
         frameHeight: 250,
-        numFrames: 7,
+        numFrames: 21,
         currentFrame: 0,
+        numRows: 3,
+        framesPerRow: 7,
+        rowHeight: 292,
         frameRate: 12,
         lastUpdate: 0,
         x: 50,
         y: 600,
         speed: 5,
 
-        draw:function() {
+        draw: function () {
             // Draw character image with camera offset
-            context.drawImage(runnerImg, this.currentFrame * this.frameWidth, 0, this.frameWidth, this.frameHeight, this.x - camera.cameraX, this.y - camera.cameraY, this.frameWidth, this.frameHeight);
+            row = Math.floor(this.currentFrame / this.framesPerRow);
+            context.drawImage(runnerImg,
+                ((this.currentFrame % this.framesPerRow) * this.frameWidth),
+                row * this.rowHeight,
+                this.frameWidth,
+                this.frameHeight,
+                this.x - camera.cameraX,
+                this.y - camera.cameraY,
+                this.frameWidth,
+                this.frameHeight);
         },
 
-        update:function(time) {
-            if(time - this.lastUpdate > 1000 / this.frameRate) {
-                this.currentFrame = (this.currentFrame + 1 ) % this.numFrames;
+        update: function (time) {
+            if (time - this.lastUpdate > 1000 / this.frameRate) {
+                this.currentFrame = (this.currentFrame + 1) % this.numFrames;
                 this.lastUpdate = time;
             }
 
-            if(keys.ArrowLeft) {
+            if (keys.ArrowLeft) {
                 this.x -= this.speed;
             }
 
-            if(keys.ArrowRight) {
+            if (keys.ArrowRight) {
                 this.x += this.speed;
             }
 
-            if(keys.ArrowUp) {
+            if (keys.ArrowUp) {
                 this.y -= this.speed;
             }
 
-            if(keys.ArrowDown) {
+            if (keys.ArrowDown) {
                 this.y += this.speed;
             }
         },
-    
+
     };
 
     function gameLoop(time) {
         context.clearRect(0, 0, canvas.width, canvas.height);
-        
+
         camera.update();
         runner.update(time);
 
@@ -110,8 +122,7 @@ window.onload = function() {
     window.addEventListener("keyup", (e) => { keys[e.key] = false; })
 
     // wait for images to load before starting the game loop
-    Promise.all([loadImage(bgImg), loadImage(runnerImg)]).then(() =>
-    { gameLoop(); });
+    Promise.all([loadImage(bgImg), loadImage(runnerImg)]).then(() => { gameLoop(); });
 
     // Load image with promise
     function loadImage(img) {
@@ -121,5 +132,5 @@ window.onload = function() {
         });
     }
 
- 
+
 }
